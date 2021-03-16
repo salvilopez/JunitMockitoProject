fc65170a43272c1847745130cedc87c114aa87b1
Salvador Sierra <salvilopezronda@gmail.com>
2021-03-16 14:35:29 +0100

    notificationtetscorrect

--- src/test/java/com/example/service/EventNotificationTest.java
+++ src/test/java/com/example/service/EventNotificationTest.java
@@ -4,6 +4,7 @@ import com.example.Attendee;
 import com.example.Event;
 import com.example.EventType;
 import com.example.Notification;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,56 +24,96 @@ import static org.junit.jupiter.api.Assertions.*;
 class EventNotificationTest {
 
 
+
+
     private static final String MSG_ANNOUNCE = "The next big event is coming!";
     private static final String MSG_CONFIRM = "Dear Attendee, your subscription to the event has been confirmed successfully.";
 
 
     @Mock
-    Event event;
+    EventNotificationServiceImpl eventService;
+
 
 
     @InjectMocks
-    EventNotificationServiceImpl eventService;
+    Event event;
 
+    //****************************            Mock                *********************************************/
+
+    @Test
+    @DisplayName("Test testAnouncementMockuno ")
+    public void testAnouncementMockuno() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    @Test
+    @DisplayName("Test announcement Mock")
+    @Disabled
+    public void testAnouncementMock() {
+        event = new Event(1l,"eventopruebabussines",EventType.BUSINESS,eventService);
+        ArgumentCaptor<Event>argumentCaptor=ArgumentCaptor.forClass(Event.class);
+        eventService.announce(event);
+
+        verify(eventService,times(1)).announce(argumentCaptor.capture());
+
+
+        assertEquals(event,argumentCaptor.getValue());
+
+    }
     //****************************            announce()                *********************************************/
+
+
 
     @Test
     @DisplayName("Test announcement")
     public void testAnouncement() {
-        event = new Event();
-        event.setId(1l);
-        event.setTitle("eventoPrueba");
+        EventNotificationServiceImpl eventServiceTmp = new EventNotificationServiceImpl();
+        Event eventTemp = new Event();
+        eventTemp.setId(1l);
+        eventTemp.setTitle("eventoPrueba");
 
-        Attendee attendee= new Attendee(1l,"salvi","salvi@gmail.com");
-        List<Attendee> attendees= new ArrayList<Attendee>();
-        List<Notification> notifications= new ArrayList<Notification>();
+        Attendee attendee = new Attendee(1l, "salvi", "salvi@gmail.com");
+        List<Attendee> attendees = new ArrayList<Attendee>();
+        List<Notification> notifications = new ArrayList<Notification>();
         attendee.setNotifications(notifications);
         attendees.add(attendee);
-        event.setAttendees(attendees);
-        eventService.announce(event);
-        assertEquals(MSG_ANNOUNCE,event.getAttendees().get(0).getNotifications().get(0).getMessage());
-
+        eventTemp.setAttendees(attendees);
+        eventServiceTmp.announce(eventTemp);
+        assertEquals(MSG_ANNOUNCE, eventTemp.getAttendees().get(0).getNotifications().get(0).getMessage());
     }
     //****************************            confirmAttendance()                ***************************************/
     @Test
     @DisplayName("Test Confirm Anouncement")
-    public void testConfirmAnouncement() {
-        event = new Event();
-        event.setId(1l);
-        event.setTitle("eventoPrueba");
+    public void testConfirmAnouncement () {
+        EventNotificationServiceImpl eventServiceTmp = new EventNotificationServiceImpl();
+        Event eventTemp = new Event();
+        eventTemp.setId(1l);
+        eventTemp.setTitle("eventoPrueba");
 
-        Attendee attendee= new Attendee(1l,"salvi","salvi@gmail.com");
-        List<Attendee> attendees= new ArrayList<Attendee>();
-        List<Notification> notifications= new ArrayList<Notification>();
+        Attendee attendee = new Attendee(1l, "salvi", "salvi@gmail.com");
+        List<Attendee> attendees = new ArrayList<Attendee>();
+        List<Notification> notifications = new ArrayList<Notification>();
         attendee.setNotifications(notifications);
         attendees.add(attendee);
-        event.setAttendees(attendees);
+        eventTemp.setAttendees(attendees);
 
 
-        eventService.confirmAttendance(event,attendee);
+        eventServiceTmp.confirmAttendance(eventTemp, attendee);
 
 
-        assertEquals(MSG_CONFIRM,event.getAttendees().get(0).getNotifications().get(0).getMessage());
+        assertEquals(MSG_CONFIRM, eventTemp.getAttendees().get(0).getNotifications().get(0).getMessage());
 
     }
 
